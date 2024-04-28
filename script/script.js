11c78e4fc21df8f07bb9c02120f5a7d5d2628097
Honmasu <112128372+Honmasu@users.noreply.github.com>
2024-04-28 15:11:20 -0400

    changement du code

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// GSAP animation for the arrow icon
 gsap.to(".scroll-icon", {
     y: -10,
     duration: 1,
@@ -5,7 +8,23 @@ gsap.to(".scroll-icon", {
     yoyo: true
 });
 
+// GSAP animation for rotating the CTA icon
+gsap.to("#cta-icon", {
+    rotation: 360,
+    duration: 2, 
+    repeat: -1,  
+    ease: "none" 
+});
 
+// GSAP animation for moving the CTA icon up and down
+gsap.to("#cta-icon", {
+    y: 10,
+    duration: 1,
+    yoyo: true,
+    repeat: -1
+});
+
+// Scroll event listener to add/remove scrolling class
 window.addEventListener('scroll', function() {
     document.body.classList.add('is-scrolling');
 
