7695cb49ef8b21e71d3569c8091c713ee95eaa7e
Honmasu <112128372+Honmasu@users.noreply.github.com>
2024-05-12 21:36:00 -0400

    keep going

---
+++ rapport-optimisation.md
@@ -0,0 +1,19 @@
+# Rapport d'optimisation du project ScrollTelling
+
+[Page Speed Insights - Rapport du 12 mai 2024, 9pm](https://michael.tim-momo.com:2083/cpsess6255754337/frontend/jupiter/index.html?login=1&post_login=61824730066025)
+[Page Speed Insights - Rapport du 12 mai 2024, 9pm](https://michael.tim-momo.com:2083/cpsess6255754337/frontend/jupiter/index.html?login=1&post_login=61824730066025)
+
+## Problème #1
+### les image prend trop de place sur mon site
+### je devrais mettres mes images en laoding lazy
+### 
+
+## Problème #2
+### les images prennent trop de volume sur le site
+### mes image png devraient être en webp
+### 
+
+# Problème #3
+### le code est trop grand
+### je devrais mettre mon code en de minify
+### j'ai mit mon code js et style en minimum ce qui a aider  l'optimisation de mon site
--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(DrawSVGPlugin);
-gsap.registerPlugin(MorphSVGPlugin);
+/*gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(MorphSVGPlugin);*/
 
 /* ---- Animation header ----- */
 gsap.timeline({
@@ -14,6 +14,8 @@ gsap.timeline({
   }
 }).to(".start", {});
 
+
+/* translate du spritesheet pod */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
@@ -27,6 +29,7 @@ gsap.timeline({
   y: '230vh'
 })
 
+/* animation de la flèche */
 gsap.to(".fleche", {
   y: '2vh',
   duration: 1,
@@ -43,10 +46,11 @@ gsap.timeline({
     markers: true,
     start: 'top 0%',
     end: '100%',
-    trigger: '#chapitre1',
+    trigger: '#Chapitre1',
   }
 }).to(".pod_solo", {});
 
+/* spritesheet du boom */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
@@ -60,6 +64,8 @@ gsap.timeline({
 });
 
 /* ---- Animation chapter 2 ----- */
+
+/* transition du monstre 1 */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
@@ -72,6 +78,7 @@ gsap.timeline({
   x: '-50vw'
 });
 
+/* transition du monstre 2 */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
@@ -84,25 +91,27 @@ gsap.timeline({
   x: '50vw'
 });
 
+/* possition du pod */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
     markers: true,
     start: 'top 0%',
-    end: '+=90%',
+    end: '90%',
     trigger: '#Chapitre1',
   }
 }).to('.pod_solo', {
   y: '20vh'
 });
 
+/*transition du nade */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
     markers: true,
     start: 'top 0%',
-    end: '+=100%',
-    trigger: '#Chapitre2',
+    end: '300%',
+    trigger: '.nade',
   }
 }).to('.nade', {
   y: '120vh',
@@ -110,29 +119,34 @@ gsap.timeline({
 });
 
 /* ---- Animation chapter 3 ----- */
+
+/* transition du monstre 3 */
 gsap.timeline({
   scrollTrigger: {
-    pin: true,
     scrub: true,
     markers: true,
-    start: 'top 20%',
-    end: '60%',
-    trigger: '#chapitre3',
+    start: 'top 100%',
+    end: '0%',
+    trigger: '#Chapitre3',
   }
 }).to(".monstre3", {});
 
+
+/* transition du monstre 3 */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
     markers: true,
-    start: 'top 20%',
-    end: '60%',
+    start: 'top 0%',
+    end: '50%',
     trigger: '#Chapitre3',
   }
 }).to('.monstre3', {
   x: '50vw'
 });
 
+
+/* transition du monstre 4 */
 gsap.timeline({
   scrollTrigger: {
     scrub: true,
@@ -146,8 +160,11 @@ gsap.timeline({
 });
 
 /* ---- Animation chapter 4 ----- */
+
+/* parallax repeat lors que l'on scroll vers le haut */
 const parallaxTimeline = gsap.timeline({
   scrollTrigger: {
+    pin: true,
     trigger: "#Chapitre4",
     start: 'top 0%',
     end: '+=50%',
@@ -160,6 +177,8 @@ parallaxTimeline.to('.layer3', { yPercent: -20 })
   .to('.layer1', { yPercent: -60 }, "-=0.5");
 
 /* ---- Animation chapter 5 ----- */
+
+/* pod suit le path 
 const path = "M9,100c0,0,85.53,-185.58,199.91,-310.11c130,-100,280.81,-148.88,330.39,-148.88c138.87,0,275.55,67.71,370.05,134.47c110.09,98.85,203.33,237.79,317.71,367.77c140.09,138.89,334.44,267.75,623.37,267.75c122.23,0,306.62,-38.83,455.84,-84.43c177.97,-64.45,342.21,-208.88,447.78,-347.7c143.35,-177.75,144.43,-263.37,144.43,-263.37";
 const tl = gsap.timeline({ repeat: -1, yoyo: true });
 tl.to(".pod2", {
@@ -173,7 +192,7 @@ tl.to(".pod2", {
   }
 });
 
-/* ---- Animation chapter 6 ----- */
+/* draw svg
 gsap.set("#soleil3", { x: 200, y: 200 });
 gsap.to("#soleil3", {
   duration: 5,
@@ -182,7 +201,7 @@ gsap.to("#soleil3", {
   yoyo: true,
 });
 
-/*-- morph -- */
+/*-- morph --
 gsap.set("#cercle", { x: -50, y: 200 });
 gsap.set("#soleil", { x: 200, y: 200 });
 
@@ -194,7 +213,7 @@ gsap.to("#cercle", {
   ease: "power1.inOut"
 });
 
-
+/*-- morph 2--
 gsap.set("#cercle2", { x: -50, y: 400 });
 gsap.set("#soleil2", { x: 200, y: 200 });
 
@@ -204,4 +223,4 @@ gsap.to("#cercle2", {
   repeat: -1,
   yoyo: true,
   ease: "power1.inOut"
-});
+});*/
